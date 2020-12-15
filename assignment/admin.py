8925c34aa97bef318f21b140b354b7163b2cf586
Aritra Mondal <mondalaritra002@gmail.com>
2020-12-15 10:47:57 +0530

    Added Class, Assignment, Question, Submission models

--- assignment/admin.py
+++ assignment/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Class, Assignment, Question, Submission, BestSubmission
+
+admin.site.register(Class)
+admin.site.register(Assignment)
+admin.site.register(Question)
+admin.site.register(Submission)
+admin.site.register(BestSubmission)
--- assignment/models.py
+++ assignment/models.py
@@ -1,3 +1,54 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
+class Class(models.Model):
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    slug = models.SlugField(max_length=10, blank=True, null=True)
+    name = models.CharField(max_length=50, null=True, blank=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.name)
+
+
+class Assignment(models.Model):
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    slug = models.SlugField(max_length=10, blank=True, null=True)
+    class_name =  models.ForeignKey(Class, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50, null=True, blank=True)
+    due_date = models.DateField(max_length=50, null=True, blank=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.name)
+
+
+class Question(models.Model):
+    assignment = models.ForeignKey(Assignment, on_delete=models.CASCADE)
+    slug = models.SlugField(max_length=10, blank=True, null=True)
+    title = models.ForeignKey(User, on_delete=models.CASCADE)
+    body = models.CharField(max_length=5000, null=True, blank=True)
+    allowed_lang = models.CharField(max_length=10, null=True, blank=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.title)
+
+
+class Submission(models.Model):
+    submitted_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    slug = models.SlugField(max_length=10, blank=True, null=True)
+    score = models.IntegerField(null=True, blank=True)
+
+    def __str__(self):
+        return str(self.slug)
+
+
+class BestSubmission(models.Model):
+    submitted_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    slug = models.SlugField(max_length=10, blank=True, null=True)
+    score = models.IntegerField(null=True, blank=True)
+
+    def __str__(self):
+        return str(self.slug)
