52f5ca20f38226c719bc6ddc30c23c252ad10a07
Aritra Mondal <mondalaritra002@gmail.com>
2020-12-18 12:06:42 +0530

    Added Auto Slug method

--- assignment/models.py
+++ assignment/models.py
@@ -1,12 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+from Assignments.utls import slug_generator
 
 class Class(models.Model):
     created_by = models.ForeignKey(User, on_delete=models.CASCADE)
     slug = models.SlugField(max_length=10, blank=True, null=True)
     name = models.CharField(max_length=50, null=True, blank=True)
     timestamp = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slug_generator()
+        super(Class, self).save(*args, **kwargs)
 
     def __str__(self):
         return str(self.name)
@@ -20,6 +25,10 @@ class Assignment(models.Model):
     due_date = models.DateField(max_length=50, null=True, blank=True)
     timestamp = models.DateTimeField(auto_now_add=True)
 
+    def save(self, *args, **kwargs):
+        self.slug = slug_generator()
+        super(Assignment, self).save(*args, **kwargs)
+
     def __str__(self):
         return str(self.name)
 
@@ -32,15 +41,23 @@ class Question(models.Model):
     allowed_lang = models.CharField(max_length=10, null=True, blank=True)
     timestamp = models.DateTimeField(auto_now_add=True)
 
+    def save(self, *args, **kwargs):
+        self.slug = slug_generator()
+        super(Question, self).save(*args, **kwargs)
+
     def __str__(self):
         return str(self.title)
 
 
 class Submission(models.Model):
-    submitted_by = models.ForeignKey(User, on_delete=models.CASCADE)
-    question = models.ForeignKey(Question, on_delete=models.CASCADE, null=True)
+    submitted_by = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, null=True, on_delete=models.CASCADE)
     slug = models.SlugField(max_length=10, blank=True, null=True)
     score = models.IntegerField(null=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slug_generator()
+        super(Submission, self).save(*args, **kwargs)
 
     def __str__(self):
         return str(self.slug)
@@ -52,12 +69,16 @@ class BestSubmission(models.Model):
     slug = models.SlugField(max_length=10, blank=True, null=True)
     score = models.IntegerField(null=True, blank=True)
 
+    def save(self, *args, **kwargs):
+        self.slug = slug_generator()
+        super(BestSubmission, self).save(*args, **kwargs)
+
     def __str__(self):
         return str(self.slug)
 
 
 class Executions(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    submission = models.OneToOneField(Submission, null=True, on_delete=models.CASCADE)
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     language = models.CharField(max_length=10, null=True, blank=True)
     verdict = models.CharField(max_length=10, null=True, blank=True)
