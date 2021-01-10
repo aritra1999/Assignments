263929808c22bac46b59fa3e03c500314dba5def
Hrishav Kumar <jha2506@gmail.com>
2021-01-10 14:21:31 +0530

    After Patch

--- Assignments/settings.py
+++ Assignments/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -86,7 +87,6 @@ DATABASES = {
     }
 }
 
-import django_heroku
 django_heroku.settings(locals())
 
 # Password validation
--- assignment/models.py
+++ assignment/models.py
@@ -123,4 +123,3 @@ class IO(models.Model):
 
     def __str__(self):
         return str(self.question)
-    
