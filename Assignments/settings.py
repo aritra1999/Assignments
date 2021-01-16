7b7113abcd539247b94ea01d953a6ca7c3c0fa85
Hrishav Kumar <jha2506@gmail.com>
2021-01-16 16:07:06 +0530

    Latest Stable Build

--- Assignments/settings.py
+++ Assignments/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 import os
 from pathlib import Path
 import django_heroku
+from django.core.mail import send_mail
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,6 +76,14 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'Assignments.wsgi.application'
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'codevoxteam@gmail.com'
+EMAIL_HOST_PASSWORD = 'Codevox@123'
 
 
 # Database
