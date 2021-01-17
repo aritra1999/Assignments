dd68025f2b4408cb71fdd093243515aadd237333
Hrishav Kumar <jha2506@gmail.com>
2021-01-17 22:44:57 +0530

    Latest Stable Build

--- static/js/landing.js
+++ static/js/landing.js
@@ -1,11 +1,11 @@
 let cursor = true
 let speed = 400
-let colors = Array("#CA4246", "#E16541", "#F18F43", "#8B9862", "#A7489B", "#476098")
+// let colors = Array("#CA4246", "#E16541", "#F18F43", "#8B9862", "#A7489B", "#476098")
 
 setInterval(() => {
     if(cursor) {
         document.getElementById('cursor').style.opacity = "0"
-        document.getElementById('cursor').style.color = colors[Math.floor(Math.random() * colors.length)]
+        // document.getElementById('cursor').style.color = colors[Math.floor(Math.random() * colors.length)]
         cursor = false
     }else {
         document.getElementById('cursor').style.opacity = "1"
