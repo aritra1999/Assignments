9d7d31d77111a96abbaaae4b9d4fa7fe2de02932
Hrishav Kumar <jha2506@gmail.com>
2021-01-18 13:41:20 +0530

    Home Page Responsive Done

--- static/js/landing.js
+++ static/js/landing.js
@@ -1,14 +1,25 @@
 let cursor = true
 let speed = 400
-// let colors = Array("#CA4246", "#E16541", "#F18F43", "#8B9862", "#A7489B", "#476098")
 
 setInterval(() => {
     if(cursor) {
         document.getElementById('cursor').style.opacity = "0"
-        // document.getElementById('cursor').style.color = colors[Math.floor(Math.random() * colors.length)]
         cursor = false
     }else {
         document.getElementById('cursor').style.opacity = "1"
         cursor = true
     }
 }, speed)
+
+
+let ham = document.querySelector('.ham')
+let navbarMob = document.querySelector('.navbarMob')
+let triggerIndicator = 1
+ham.addEventListener("click", openFunc)
+function openFunc(){
+    triggerIndicator++
+    if(triggerIndicator%2===0)
+        navbarMob.classList.add("open")
+    else
+    navbarMob.classList.remove("open")
+}
