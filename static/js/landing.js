a7a1da5114e5e047190cb1ceeb1aea43fb064ffa
Hrishav <jha2506@gmail.com>
2021-02-20 14:29:00 +0530

    About page added

--- static/js/landing.js
+++ static/js/landing.js
@@ -1,25 +1,28 @@
-let cursor = true
-let speed = 400
+if (document.getElementById('cursor')) {
+    let cursor = true
+    let speed = 400
 
-setInterval(() => {
-    if(cursor) {
-        document.getElementById('cursor').style.opacity = "0"
-        cursor = false
-    }else {
-        document.getElementById('cursor').style.opacity = "1"
-        cursor = true
-    }
-}, speed)
+    setInterval(() => {
+        if (cursor) {
+            document.getElementById('cursor').style.opacity = "0"
+            cursor = false
+        } else {
+            document.getElementById('cursor').style.opacity = "1"
+            cursor = true
+        }
+    }, speed)
+}
 
 
 let ham = document.querySelector('.ham')
 let navbarMob = document.querySelector('.navbarMob')
 let triggerIndicator = 1
 ham.addEventListener("click", openFunc)
-function openFunc(){
+
+function openFunc() {
     triggerIndicator++
-    if(triggerIndicator%2===0)
+    if (triggerIndicator % 2 === 0)
         navbarMob.classList.add("open")
     else
-    navbarMob.classList.remove("open")
+        navbarMob.classList.remove("open")
 }
