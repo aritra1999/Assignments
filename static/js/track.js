cb29ce1ab6f6e4f2d296b708c69d3ce0410a03e1
Aritra Mondal <mondalaritra002@gmail.com>
2021-05-11 19:44:27 +0530

    Disabled external copy paste

--- static/js/track.js
+++ static/js/track.js
@@ -1,4 +1,3 @@
-
 document.addEventListener( 'visibilitychange' , function() {
     let today = new Date();
     let dateTime = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate()+' '+ today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
@@ -18,3 +17,30 @@ document.addEventListener( 'visibilitychange' , function() {
     }
     localStorage.setItem('activity', JSON.stringify(log));
 }, false );
+
+
+// Prevent Right click 
+document.addEventListener('contextmenu', event => event.preventDefault());
+
+// Prevent external copy paste
+document.body.addEventListener("keydown",function(e){
+    e = e || window.event;
+    let key = e.which || e.keyCode; // keyCode detection
+    let ctrl = e.ctrlKey ? e.ctrlKey : ((key === 17) ? true : false); // ctrl detection
+
+    if ( key == 86 && ctrl ) {
+        e.preventDefault();
+        let clipboard_text = localStorage.getItem("clipboard");
+        editor.session.insert(editor.getCursorPosition(), clipboard_text);
+
+    } else if ( key == 67 && ctrl ) {
+        e.preventDefault();
+        let selected = editor.getSession().doc.getTextRange(editor.selection.getRange());
+        localStorage.setItem("clipboard", selected);
+    } else if ( key == 88 && ctrl){
+        e.preventDefault();
+        let selected = editor.getSession().doc.getTextRange(editor.selection.getRange());
+        editor.session.replace(editor.selection.getRange(), "");  
+        localStorage.setItem("clipboard", selected);
+    }
+},false);
