50b2e2b61be0ee8439877f359dcb9e55f0d50b3c
Hrishav <jha2506@gmail.com>
2021-06-26 19:59:28 +0530

    Update assignments.js

--- static/js/assignments.js
+++ static/js/assignments.js
@@ -4,17 +4,42 @@ let assignmentContainer = document.getElementById("assignmentContainer")
 let studentContainer = document.getElementById("studentContainer")
 
 assignmentToggle.addEventListener("click", openAssignment)
-function openAssignment(){
-    assignmentContainer.style.display="block"
-    studentContainer.style.display="none"
+
+function openAssignment() {
+    assignmentContainer.style.display = "block"
+    studentContainer.style.display = "none"
     assignmentToggle.classList.add("activeTag")
     studentToggle.classList.remove("activeTag")
 }
 
 studentToggle.addEventListener("click", openStudent)
-function openStudent(){
-    assignmentContainer.style.display="none"
-    studentContainer.style.display="block"
+
+function openStudent() {
+    assignmentContainer.style.display = "none"
+    studentContainer.style.display = "block"
     assignmentToggle.classList.remove("activeTag")
     studentToggle.classList.add("activeTag")
+}
+
+function AddIO() {
+    let IOField = $("#inputOutputFieldCont")
+    if(testCaseNumber <= 5){
+        IOField.append(`<div class="questionCreateFormElem" id="inputOutputField">
+                            <h2 class="sampleText">Sample Test Case ${testCaseNumber}</h2>
+                                <div class="sample">
+                                    <label class="assignmentInfo sampleTest" for="questionName">Input ${testCaseNumber}:
+                                        <textarea aria-required="true" name="input${testCaseNumber}" rows="10" cols="50" class="assignmentInfo" required></textarea>
+                                    </label>
+                                    <label class="assignmentInfo sampleTest" for="questionName">Output ${testCaseNumber}:
+                                        <textarea aria-required="true" name="output${testCaseNumber}" rows="10" cols="50" class="assignmentInfo" required></textarea>
+                                    </label>
+                                </div>
+                                <label class="score" for="score">Score ${testCaseNumber}(Out of 100):
+                                    <input type="number" class="score" placeholder="Score" required name="score${testCaseNumber}">
+                                </label>
+                            </div>`)
+    testCaseNumber++
+    } else {
+        document.getElementById("addTC").style.display="none"
+    }
 }
